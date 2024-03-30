7b1c9572db25562ab8e3c382c1c532ac303caa0c
khadijatul-kubra <khadijatulkubra@gmail.com>
2024-03-30 12:04:49 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('.splide' ,{
+        perPage: 5,
+        breakpoints: {
+            992: {
+                perPage: 4,
+            },
+            768: {
+                perPage: 3,
+            },
+            400: {
+                perPage: 2,
+            },
+
+            
+        },
+        rewind : true,
+        pagination:false,
+    });
+    splide.mount();
+});
+
+const cartbtn = document.getElementById('add-cart')
+
+function addcartfunction(){
+    cartbtn.innerHTML = "Added to cart"
+}
+
+
+
+
